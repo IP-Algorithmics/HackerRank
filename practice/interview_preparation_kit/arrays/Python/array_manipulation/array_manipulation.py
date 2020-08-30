ed1faa961f2b7e2b21719edcba1ffb2cbbd0ec2c
Iulian Preda <iulian.octavian.preda@gmail.com>
2020-08-30 20:27:58 +0300

    Interview Preparation Kit - Arrays - Array manipulation

--- practice/interview_preparation_kit/arrays/Python/array_manipulation/array_manipulation.py
+++ practice/interview_preparation_kit/arrays/Python/array_manipulation/array_manipulation.py
@@ -10,12 +10,29 @@ import sys
 
 
 def arrayManipulation(n, queries):
+    arr = [0]*n
+    for query in queries:
+        arr[query[0] - 1] += query[2]
+        arr[query[1] - 1] -= query[2]
+    return sum(arr)
+
+
+def arrayManipulationNaive2(n, queries):
+    arr = [0]*n
+    for query in queries:
+        for i in range(query[0] - 1, query[1]):
+            arr[i] += query[2]
+    return max(arr)
+
+
+def arrayManipulationNaive1(n, queries):
+    # O(n*m) implementation
     maxValue = 0
     for i in range(1, n+1):
         localMax = 0
-        for querry in queries:
-            if(i >= querry[0] and i <= querry[1]):
-                localMax += querry[2]
+        for query in queries:
+            if(i >= query[0] and i <= query[1]):
+                localMax += query[2]
         if(localMax > maxValue):
             maxValue = localMax
     return maxValue
