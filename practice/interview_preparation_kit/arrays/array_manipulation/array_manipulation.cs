b5082193e4c0d659180618494c53c31dd7810cf5
Iulian Preda <iulian.octavian.preda@gmail.com>
2020-08-30 21:35:01 +0300

    Moved files

---
+++ practice/interview_preparation_kit/arrays/array_manipulation/array_manipulation.cs
@@ -0,0 +1,20 @@
+
+using System;
+using Xunit;
+
+class ArrayManipulation
+{
+    void Arr(string[] args)
+    {
+        Console.WriteLine("Hello...");
+    }
+
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
